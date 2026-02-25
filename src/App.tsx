8223bb75b461fbdf73fb7018b02b081c717346a3
vladislav_dobrinsky <vdobrinskiy@sdvor.com>
2026-02-25 14:59:18 +0500

    feat: footer header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CharactersList, CharacterDetail } from './pages';
+import { Header, Footer } from './components';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<CharactersList />} />
-        <Route path="/character/:id" element={<CharacterDetail />} />
-      </Routes>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<CharactersList />} />
+            <Route path="/character/:id" element={<CharacterDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p className="footer-text">Made with love by TTeJlMeH4ik_92 </p>
+      </div>
+    </footer>
+  );
+};
